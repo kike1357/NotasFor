58eca0caea72ef63d5d99bfe4fad8b1d1c415ad1
nlopezjimenez <nlopezjimenez@atanasoff23.danielcastelao.org>
2015-11-12 08:53:48 +0100

    Commit Ejercicio For Main

--- src/EjercicioExamenFor/EjercicioExamenFor.java
+++ src/EjercicioExamenFor/EjercicioExamenFor.java
@@ -15,14 +15,15 @@ public class EjercicioExamenFor {
        float nota1,nota2,nota3;
        String alumno;
        float calcularNotaMedia;
-       for(i=0;i<30;){
+       for(i=0;i<30;i++){
        Examen n= new Examen();
        alumno=n.Alumno();
-       nota1=n.getNota1();
-       nota2=n.getNota2();
-       nota3=n.getNota3();
+       nota1=n.getNota();
+       nota2=n.getNota();
+       nota3=n.getNota();
       calcularNotaMedia= n.calcularNotaMedia(nota1,nota2,nota3);
        n.NotaMedia(calcularNotaMedia,alumno);
+       
     }
     
 }
